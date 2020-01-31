31af22d6fd75b5943d9b1ecdf57bf0799ac86027
Eduardo Francis Batista <eduardoefb@gmail.com>
2020-01-31 10:57:05 -0300

    radio_access_db_extractor

--- files/scripts/nelogcol/sftp.py
+++
@@ -1,5 +0,0 @@
-import pysftp
-
-with pysftp.Connection('10.20.0.2', username='SYSTEM', password='SYSTEM') as sftp:
-    with sftp.cd('/ASG_6_11_0/LICENCE'):           # temporarily chdir to allcode        
-        sftp.get('*.XML')         # get a remote file
--- files/scripts/nelogcol/usrpw.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-
-class usrpw:
-	def __init__(self, _user, _pass):
-			self._user = _user
-			self._pass = _pass
-		
